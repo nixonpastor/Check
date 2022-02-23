021dd5f43cdfdfaeb9c1aa3d9b0b2b35ae486813
Nixon Pastor <nixonpastorguzman@gmail.com>
2022-02-23 01:47:57 -0600

    Add Custom timer functionality

--- Check/View/TimerView.swift
+++ Check/View/TimerView.swift
@@ -13,7 +13,11 @@ struct TimerView: View {
     @State var to : Int = 1500
     @State var count = 0
     @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State var selectedPicker : Int = 0
+    @State var selectedPicker : Int = 3
+    @State var hours: Int = 0
+    @State var minutes: Int = 0
+    @State var seconds: Int = 0
+    @State var isSelected = false
     
     init(){
         UISegmentedControl.appearance().backgroundColor = UIColor(Color(0x070707))
@@ -55,12 +59,18 @@ struct TimerView: View {
     }
     
     
+    
     func countToMinutes() -> String {
            let currentTime = to - count
-           let seconds = currentTime % 60
-           let minutes = Int(currentTime / 60)
-           
-           return "\(minutes):\(seconds < 10 ? "0" : "")\(seconds)"
+           let hours = currentTime / 3600
+           let minutes = (currentTime % 3600)/60
+           let seconds = (currentTime % 3600)%60
+        
+        if(hours > 0){
+            return "\(hours):\(minutes < 10 ? "0" : "")\(minutes):\(seconds < 10 ? "0" : "")\(seconds)"
+        }
+        
+           return "\(minutes < 10 ? "0" : "")\(minutes):\(seconds < 10 ? "0" : "")\(seconds)"
        }
     
     func countToProgress() -> CGFloat {
@@ -71,38 +81,19 @@ struct TimerView: View {
             return countToProgress() == 1
         }
     
-    @ViewBuilder
-    func CustomTimerView() -> some View{
+    func getSeconds() -> Int{
+        let currentHours = hours * 3600
+        let currentMinutes = minutes * 60
         
-        Text("Testing Custom Timer View Function").foregroundColor(.white)
-
-        ZStack{
-            Circle().trim(from: 0, to: 1)
-                .stroke(Color.white.opacity(0.09), style: StrokeStyle(lineWidth: 35, lineCap: .round))
-                .frame(width: 280, height: 280)
-
-            Circle().trim(from: 0, to: countToProgress())
-                .stroke(Color.red, style: StrokeStyle(lineWidth: 35, lineCap: .round))
-                .frame(width: 280, height: 280)
-                .rotationEffect(.init(degrees: -90))
-
-
-            VStack{
-                Text("\(self.count)")
-                    .font(.system(size: 65))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-
-            }
-        }.padding(.top, 100)
+        return currentHours + currentMinutes + seconds
     }
     
     @ViewBuilder
-    func PomodoroTimerView() -> some View{
+    func CustomTimerView() -> some View{
         ZStack{
             Circle()
                         .fill(Color.clear)
-                        .frame(width: 350, height: 350)
+                        .frame(width: 400, height: 400)
                         .overlay(
                             Circle().stroke(Color.white.opacity(0.09), lineWidth: 15)
                             
@@ -110,7 +101,202 @@ struct TimerView: View {
             
             Circle()
                         .fill(Color.clear)
-                        .frame(width: 350, height: 350)
+                        .frame(width: 400, height: 400)
+                        .overlay(
+                            Circle().trim(from:0, to: countToProgress())
+                                .stroke(
+                                    style: StrokeStyle(
+                                        lineWidth: 15,
+                                        lineCap: .round,
+                                        lineJoin:.round
+                                    )
+                            )
+                                .rotationEffect(.init(degrees: -90))
+                                .foregroundColor(
+                                    (completed() ? Color.green : Color.orange)
+                            ).animation(
+                                .easeInOut(duration: 0.2)
+                            )
+                    )
+
+
+            VStack{
+                isSelected ?
+                AnyView(
+                    VStack{
+                    Text(countToMinutes())
+                    .font(.system(size: 65))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.top, 70)
+                        
+                        Button(action: {
+                            isSelected = false
+                            self.to = -1
+                            seconds = 0
+                            hours = 0
+                            minutes = 0
+                            self.count = 0
+                            start.toggle()
+                        }){
+                            HStack{
+                            
+                            Text("Reset")
+                                .foregroundColor(Color.red)
+                        }
+                        .padding(.vertical)
+                                                }
+                        
+                    }
+                
+                )
+                :
+                AnyView(
+                    HStack {
+                    Spacer()
+                                Picker("", selection: $hours){
+                                    ForEach(0..<24, id: \.self) { i in
+                                        Text("\(i) hours").tag(i).foregroundColor(.white)
+                                    }
+                                }.pickerStyle(WheelPickerStyle())
+                            .frame(width: 110).clipped().compositingGroup()
+                    
+                                Picker("", selection: $minutes){
+                                    ForEach(0..<60, id: \.self) { i in
+                                        Text("\(i) min").tag(i)
+                                    }.foregroundColor(.white)
+                                }.pickerStyle(WheelPickerStyle())
+                            .frame(width: 110).clipped().compositingGroup()
+                    
+                                Picker("", selection: $seconds){
+                                    ForEach(0..<60, id: \.self) { i in
+                                        Text("\(i) sec").tag(i)
+                                    }.foregroundColor(.white)
+                                }.pickerStyle(WheelPickerStyle())
+                            .frame(width: 110).clipped().compositingGroup()
+                    Spacer()
+            }.padding(.horizontal)
+                    .padding(.top, -10)
+                    )
+
+            }
+        }.onReceive(self.timer) { (time) in
+            if(self.start){
+
+                if (self.count <= self.to - 1 ) {
+                    print("count2")
+                    print(self.count)
+                    print("to2")
+                    print(self.to - 1 )
+                    if (self.count == self.to){
+                        withAnimation(Animation.default){
+                            self.count = 0
+                            self.start.toggle()
+                            isSelected = false
+                        }
+                    }
+                    else{
+                        self.count += 1
+                    }
+                }
+                else{
+                    withAnimation(.default){
+                        self.to = -1
+                        self.count = 0
+                        isSelected = false
+                        seconds = 0
+                        minutes = 0
+                        hours = 0
+                    }
+                    self.start.toggle()
+                }
+                
+               
+            }
+        }
+        .onAppear{
+            isSelected = false
+            self.to = -1
+            self.count = 0
+            self.start = false
+            minutes = 0
+            hours = 0
+            seconds = 0
+        }
+        .padding(.top, 50)
+
+        
+        HStack(spacing: 20){
+            Button(action: {
+                if(getSeconds() > 0){
+                    self.to = getSeconds()
+                    isSelected = true
+                }
+                if(self.count == self.to){
+                    self.count = 0
+                    withAnimation(.default){
+                        self.to = -1
+                    }
+                }
+                self.start.toggle()
+            }){
+                HStack(spacing: 15){
+                    Image(systemName: self.start ? "pause.fill" : "play.fill")
+                        .foregroundColor(.white)
+                    Text(self.start ? "Pause" : "Play")
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical)
+                .frame(width: (UIScreen.main.bounds.width / 2) - 55)
+                .background(Color.green)
+                .clipShape(Capsule())
+                .shadow(radius: 6)
+
+
+            }
+            .shadow(radius: 6)
+
+            Button(action: {
+                
+                if(start == true){
+                    start.toggle()
+                }
+                self.count = 0
+                self.to = self.to
+            }){
+                HStack(spacing: 15){
+                Image(systemName: "arrow.clockwise")
+                    .foregroundColor(Color.green)
+                Text("Restart")
+                    .foregroundColor(Color.green)
+            }
+            .padding(.vertical)
+            .frame(width: (UIScreen.main.bounds.width / 2) - 55)
+
+            .background(
+            Capsule()
+                .stroke(Color.green, lineWidth: 2))
+            }
+            
+            
+        }
+        .padding(.top, 90)
+    }
+    
+    @ViewBuilder
+    func PomodoroTimerView() -> some View{
+        ZStack{
+            Circle()
+                        .fill(Color.clear)
+                        .frame(width: 400, height: 400)
+                        .overlay(
+                            Circle().stroke(Color.white.opacity(0.09), lineWidth: 15)
+                            
+                    )
+            
+            Circle()
+                        .fill(Color.clear)
+                        .frame(width: 400, height: 400)
                         .overlay(
                             Circle().trim(from:0, to: countToProgress())
                                 .stroke(
@@ -169,7 +355,7 @@ struct TimerView: View {
             self.count = 0
             self.start = false
         }
-        .padding(.top, 100)
+        .padding(.top, 50)
         
         HStack(spacing: 20){
             Button(action: {
@@ -199,6 +385,9 @@ struct TimerView: View {
             .shadow(radius: 6)
 
             Button(action: {
+                if(start == true){
+                    start.toggle()
+                }
                 self.count = 0
                 self.to = 1500
             }){
@@ -224,7 +413,7 @@ struct TimerView: View {
         ZStack{
             Circle()
                         .fill(Color.clear)
-                        .frame(width: 350, height: 350)
+                        .frame(width: 400, height: 400)
                         .overlay(
                             Circle().stroke(Color.white.opacity(0.09), lineWidth: 15)
                             
@@ -232,7 +421,7 @@ struct TimerView: View {
             
             Circle()
                         .fill(Color.clear)
-                        .frame(width: 350, height: 350)
+                        .frame(width: 400, height: 400)
                         .overlay(
                             Circle().trim(from:0, to: countToProgress())
                                 .stroke(
@@ -287,7 +476,7 @@ struct TimerView: View {
             self.count = 0
             self.start = false
         }
-        .padding(.top, 100)
+        .padding(.top, 50)
         
         HStack(spacing: 20){
             Button(action: {
@@ -317,6 +506,9 @@ struct TimerView: View {
             .shadow(radius: 6)
 
             Button(action: {
+                if(start == true){
+                    start.toggle()
+                }
                 self.count = 0
                 self.to = 300
             }){
@@ -342,7 +534,7 @@ struct TimerView: View {
         ZStack{
             Circle()
                         .fill(Color.clear)
-                        .frame(width: 350, height: 350)
+                        .frame(width: 400, height: 400)
                         .overlay(
                             Circle().stroke(Color.white.opacity(0.09), lineWidth: 15)
                             
@@ -350,7 +542,7 @@ struct TimerView: View {
             
             Circle()
                         .fill(Color.clear)
-                        .frame(width: 350, height: 350)
+                        .frame(width: 400, height: 400)
                         .overlay(
                             Circle().trim(from:0, to: countToProgress())
                                 .stroke(
@@ -405,7 +597,7 @@ struct TimerView: View {
             self.count = 0
             self.start = false
         }
-        .padding(.top, 100)
+        .padding(.top, 50)
         
         HStack(spacing: 20){
             Button(action: {
@@ -435,6 +627,9 @@ struct TimerView: View {
             .shadow(radius: 6)
 
             Button(action: {
+                if(start == true){
+                    start.toggle()
+                }
                 self.count = 0
                 self.to = 900
             }){
@@ -462,4 +657,5 @@ struct TimerView_Previews: PreviewProvider {
         TimerView()
     }
 }
+
 
